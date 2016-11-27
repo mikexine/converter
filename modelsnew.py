946e3490265ee17555fd03d24cb01bbaa7509904
mikexine <mikexine@@gmail.com>
2016-11-27 17:07:04 +0100

    one more table..

---
+++ modelsnew.py
@@ -0,0 +1,65 @@
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import config
+
+DeclarativeBase = declarative_base()
+
+
+def db_connect():
+    return create_engine(config.DB_URL)
+
+
+def create_db_session(engine):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def create_tables(engine):
+    DeclarativeBase.metadata.create_all(engine)
+
+
+class Tweets(DeclarativeBase):
+    __tablename__ = "tw_users"
+
+    id = Column(BigInteger, primary_key=True)
+    tweetid = Column(BigInteger)
+    published_at = Column('published_at', DateTime)
+    tweet_text = Column('tweet_text', String)
+    lang = Column('lang', String)
+    source = Column('source', String, nullable=True)
+    mentions = Column('mentions', String, nullable=True)
+    in_reply_to_status_id = Column('in_reply_to_status_id',
+                                   BigInteger, nullable=True)
+    in_reply_to_user_id = Column('in_reply_to_user_id',
+                                 BigInteger, nullable=True)
+    positive = Column('positive', Boolean, nullable=True)
+    negative = Column('negative', Boolean, nullable=True)
+    neutral = Column('neutral', Boolean, nullable=True)
+    retweeted = Column('retweeted', Boolean, nullable=True)
+    retweeted_id = Column('retweeted_id', BigInteger, nullable=True)
+    quoted = Column('quoted', Boolean, nullable=True)
+    quoted_id = Column('quoted_id', BigInteger, nullable=True)
+    user_id = Column('user_id', BigInteger)
+    created_at = Column('created_at', DateTime, nullable=True)
+    name = Column('name', String)
+    screen_name = Column('screen_name', String)
+    description = Column('description', String, nullable=True)
+    location = Column('location', String, nullable=True)
+    usr_lang = Column('usr_lang', String, nullable=True)
+
+    favourites_count = Column('favourites_count', BigInteger, nullable=True)
+    followers_count = Column('followers_count', BigInteger, nullable=True)
+    following_count = Column('following_count', BigInteger, nullable=True)
+    statuses_count = Column('statuses_count', BigInteger, nullable=True)
+
+    verified = Column('verified', Boolean, nullable=True)
+    geo_enabled = Column('geo_enabled', Boolean, nullable=True)
+    adidas = Column('adidas', Boolean, nullable=True)
+    nike = Column('nike', Boolean, nullable=True)
+    barcelona = Column('barcelona', Boolean, nullable=True)
+    real = Column('real', Boolean, nullable=True)
+    messi = Column('messi', Boolean, nullable=True)
+    ronaldo = Column('ronaldo', Boolean, nullable=True)
+
---
+++ v2smartconvert.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+import arrow
+from var_utils import start_t, print_time_str, time_str
+import sys
+from tweet_utils import sentiment_analysis
+from telegram import Bot
+from modelsnew import Tweets, db_connect, create_db_session, create_tables
+import config
+import importlib
+import traceback
+
+chat_id = config.tgchat
+bot = Bot(token=config.tgtoken)
+st = start_t()
+
+db_engine = db_connect()
+create_tables(db_engine)
+db_session = create_db_session(db_engine)
+
+
+def main():
+    parsed = 1
+    rows_inserted = 0
+    with open(sys.argv[1]) as f:
+        for line in f:
+            inserted = False
+
+            try:
+                doc = json.loads(line)
+            except:
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                lines = traceback.format_exception(exc_type, exc_value,
+                                                   exc_traceback)
+                txt = ''.join('' + line for line in lines)
+                print(txt)
+                bot.sendMessage(chat_id=chat_id, text=str(txt))
+                doc = {"limit": {"track": "err"}}
+
+            if doc.get('limit') is None:
+                tweet_id = doc.get('id')
+
+                t_published_at = arrow.get(int(doc.get('timestamp_ms')) / 1000
+                                           ).format('YYYY-MM-DD HH:mm:ss ZZ')
+                t_tweet_text = doc.get('text')
+                t_lang = doc.get('lang')
+                t_source = doc.get('source',
+                                   "").partition('>')[-1].rpartition('<')[0]
+                t_mentions = []
+
+                if doc.get('entities').get('user_mentions'):
+                    for m in doc.get('entities').get('user_mentions'):
+                        t_mentions.append(m.get('screen_name'))
+
+                t_in_reply_to_status_id = doc.get('in_reply_to_status_id')
+                t_in_reply_to_user_id = doc.get('in_reply_to_user_id')
+
+                if t_lang == "en":
+                    sentiment = sentiment_analysis(doc)
+                    if sentiment == "Positive":
+                        t_positive = True
+                        t_negative = False
+                        t_neutral = False
+                    elif sentiment == "Negative":
+                        t_positive = False
+                        t_negative = True
+                        t_neutral = False
+                    elif sentiment == "Neutral":
+                        t_positive = False
+                        t_negative = False
+                        t_neutral = True
+                else:
+                    t_positive = None
+                    t_negative = None
+                    t_neutral = None
+
+                if doc.get("retweeted_status"):
+                    t_retweeted = True
+                    t_retweeted_id = doc.get("retweeted_status").get('id')
+                else:
+                    t_retweeted = None
+                    t_retweeted_id = None
+
+                if doc.get("quoted_status"):
+                    t_quoted = True
+                    t_quoted_id = doc.get("quoted_status").get('id')
+                else:
+                    t_quoted = None
+                    t_quoted_id = None
+
+                u_id = doc.get('user').get('id')
+
+                try:
+                    u_created_at = arrow.get(doc.get('user').get('created_at'), "ddd MMM DD HH:mm:ss Z YYYY").format('YYYY-MM-DD HH:mm:ss ZZ')
+                except:
+                    u_created_at = None
+                    bot.sendMessage(chat_id=chat_id, text="date error user")
+                u_name = doc.get('user').get('name')
+                u_screen_name = doc.get('user').get('screen_name')
+                u_description = doc.get('user').get('description')
+                u_location = doc.get('user').get('location')
+                u_lang = doc.get('user').get('lang')
+                u_favourites_count = doc.get('user').get('favourites_count')
+                u_followers_count = doc.get('user').get('followers_count')
+                u_following_count = doc.get('user').get('friends_count')
+                u_statuses_count = doc.get('user').get('statuses_count')
+                u_verified = doc.get('user').get('verified')
+                u_geo_enabled = doc.get('user').get('geo_enabled')
+
+                keys = {}
+
+                for k in config.keywords:
+                    if any(w in line.lower() for w in config.keywords[k]):
+                        keys[k] = True
+              
+                tweet = Tweets(tweetid=tweet_id,
+                               published_at=t_published_at,
+                               tweet_text=t_tweet_text,
+                               lang=t_lang,
+                               source=t_source,
+                               mentions=t_mentions,
+                               in_reply_to_status_id=t_in_reply_to_status_id,
+                               in_reply_to_user_id=t_in_reply_to_user_id,
+                               positive=t_positive,
+                               negative=t_negative,
+                               neutral=t_neutral,
+                               retweeted=t_retweeted,
+                               retweeted_id=t_retweeted_id,
+                               quoted=t_quoted,
+                               quoted_id=t_quoted_id,
+                               user_id=u_id,
+                               created_at=u_created_at,
+                               name=u_name,
+                               screen_name=u_screen_name,
+                               description=u_description,
+                               location=u_location,
+                               usr_lang=u_lang,
+                               favourites_count=u_favourites_count,
+                               followers_count=u_followers_count,
+                               following_count=u_following_count,
+                               statuses_count=u_statuses_count,
+                               verified=u_verified,
+                               geo_enabled=u_geo_enabled,
+                               adidas = keys.get('adidas', False),
+                               nike = keys.get('nike', False),
+                               barcelona = keys.get('barcelona', False),
+                               real = keys.get('real', False),
+                               messi = keys.get('messi', False),
+                               ronaldo = keys.get('ronaldo', False))
+
+                db_session.add(tweet)
+                rows_inserted += 1
+
+                if not parsed % config.n_commit:
+                    db_session.commit()
+                    print_time_str(st, parsed, sys.argv[1], rows_inserted)
+
+                if not parsed % config.n_send:
+                    try:
+                        bot.sendMessage(chat_id=chat_id,
+                                        text=time_str(st, parsed, sys.argv[1],
+                                                      rows_inserted))
+                    except:
+                        pass
+
+                parsed += 1
+
+    db_session.commit()
+
+    final = "%s terminated!" % sys.argv[1]
+    bot.sendMessage(chat_id=chat_id, text=final)
+    bot.sendMessage(chat_id=chat_id, text=time_str(st, parsed, sys.argv[1],
+                                                   rows_inserted))
+
+    print_time_str(st, parsed, sys.argv[1], rows_inserted)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines1 = traceback.format_exception(exc_type)
+        lines2 = traceback.format_exception(exc_value)
+        lines3 = traceback.format_exception(exc_traceback)
+        try:
+            bot.sendMessage(chat_id=chat_id, text="err on " + sys.argv[1])
+            bot.sendMessage(chat_id=chat_id, text=str(lines1))
+            bot.sendMessage(chat_id=chat_id, text=str(lines2))
+            bot.sendMessage(chat_id=chat_id, text=str(lines3))
+        except:
+            pass
